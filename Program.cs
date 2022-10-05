e4a9145bafdb456aa0d84c550b808ddd3c3a2633
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-05 18:25:10 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProblem
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welsome to Line Comparison Computation Program");
+        }
+    }
+}
