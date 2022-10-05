e3e5fddfe7d87f93802787708ead5b720b7e7afa
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-05 19:11:59 +0530

    Comapare Length of 2 Lines with Equals Method

--- Length.cs
+++ Length.cs
@@ -18,8 +18,20 @@ namespace LineComparisonProblem
             int x2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the value of y2:");
             int y2 = Convert.ToInt32(Console.ReadLine());
-            double Length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-            Console.WriteLine("The Length of Line is: "+ Length);
+            Console.WriteLine("Enter the value of x3:");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y3:");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x4:");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of y4:");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+            double Length1 = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            double Length2 = Math.Sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+            Console.WriteLine("The Length of Line is: "+ Length1);
+            Console.WriteLine("The Length of Line is: " + Length2);
+
+            Console.WriteLine(Length1.Equals(Length2));
 
         }
     }
